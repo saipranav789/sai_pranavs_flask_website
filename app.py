0b3f4e14161450c6af739f838b00adfa5bb9ab6d
Sai Pranav <113039239+saipranav789@users.noreply.github.com>
2023-03-02 02:54:34 +0000

    reuploading after a few changes so website can be hosted by render

--- app.py
+++ app.py
@@ -2,11 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  "Backend Developer", "Frontend Developer", "Full Stack Developer",
+  "Data Analyst", "Software Development Engineer"
+]
+
 
 @app.route("/")
-def hello_world():
-  return render_template("home.html")
+def hello_web():
+  return render_template("home.html", jobs=JOBS)
 
+
+# @app.route("/contact_me")
+# def hello_contact():
+#   return render_template("contact_me.html")
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
